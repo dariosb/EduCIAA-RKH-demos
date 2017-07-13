70bbe6ecb1b57df2ec9987d59b3322c163d6042a
Dario <dariosb@gmail.com>
2017-07-13 10:30:07 -0300

    oven1 demo, must adapt leds and switch access

---
+++ oven/ov1/bsp/src/bsp.c
@@ -0,0 +1,176 @@
+/*
+ *  --------------------------------------------------------------------------
+ *
+ *                                Framework RKH
+ *                                -------------
+ *
+ *            State-machine framework for reactive embedded systems
+ *
+ *                      Copyright (C) 2010 Leandro Francucci.
+ *          All rights reserved. Protected by international copyright laws.
+ *
+ *
+ *  RKH is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any
+ *  later version.
+ *
+ *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with RKH, see copying.txt file.
+ *
+ *  Contact information:
+ *  RKH web site:   http://sourceforge.net/projects/rkh-reactivesys/
+ *  e-mail:         francuccilea@gmail.com
+ *  ---------------------------------------------------------------------------
+ */
+
+/**
+ *  \file       bsp.c
+ *  \brief      BSP for Cortex-M3 EDU-CIAA LPC4337 ARM-GCC
+ *
+ *  \ingroup    bsp
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2017.06.23  DaBa  v1.0.00  Initial version
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  DaBa  Dario Baliña       dariosb@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
+#include "rkh.h"
+#include "rkhfwk_sched.h"
+#include "rkhfwk_dynevt.h"
+#include "oven.h"
+#include "bsp.h"
+#include "leds.h"
+#include "switch.h"
+#include "sapi.h"
+
+RKH_THIS_MODULE
+
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+static RKH_TS_T tstamp;
+
+#if defined(RKH_USE_TRC_SENDER)
+static rui8_t door;
+static rui8_t panel;
+#endif
+
+static RKH_ROM_STATIC_EVENT(e_start, START);
+static RKH_ROM_STATIC_EVENT(e_open, OPEN);
+static RKH_ROM_STATIC_EVENT(e_close, CLOSE);
+static RKH_ROM_STATIC_EVENT(e_term, TERM);
+#if (__STOP_BUTTON__ == RKH_ENABLED)
+static RKH_ROM_STATIC_EVENT(e_stop, STOP);
+#endif
+
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+/* ---------------------------- Global functions --------------------------- */
+void
+bsp_timeTick(void)
+{
+    ++tstamp;
+    switch_tick();
+}
+
+RKH_TS_T
+rkh_trc_getts(void)
+{
+    return tstamp;
+}
+
+void
+bsp_init(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    boardConfig();
+
+    gpioConfig(0, GPIO_ENABLE);
+
+    gpioConfig(LED1, GPIO_OUTPUT);
+
+    rkh_fwk_init();
+
+    RKH_FILTER_ON_GROUP(RKH_TRC_ALL_GROUPS);
+    RKH_FILTER_ON_EVENT(RKH_TRC_ALL_EVENTS);
+    RKH_FILTER_OFF_EVENT(RKH_TE_TMR_TOUT);
+    RKH_FILTER_OFF_EVENT(RKH_TE_SM_STATE);
+    RKH_FILTER_OFF_SMA(oven);
+    RKH_FILTER_OFF_ALL_SIGNALS();
+
+    RKH_TRC_OPEN();
+
+#if defined(RKH_USE_TRC_SENDER)
+    RKH_TR_FWK_OBJ(&panel);
+    RKH_TR_FWK_OBJ(&door);
+#endif
+
+    RKH_ENA_INTERRUPT();
+}
+
+void
+bsp_oven_init(void)
+{
+    init_leds();
+    init_switch();
+}
+
+void
+bsp_publishSwitchEvt(ruint s, ruint debsw)
+{
+    switch (s)
+    {
+        case START_SW:
+            RKH_SMA_POST_FIFO(oven, &e_start, &panel);
+            break;
+
+#if (__STOP_BUTTON__ == RKH_ENABLED)
+        case STOP_SW:
+            RKH_SMA_POST_FIFO(oven, &e_stop, &panel);
+            break;
+#endif
+
+        case DOOR_SW:
+            if (debsw == SW_PRESS)
+            {
+                RKH_SMA_POST_FIFO(oven, &e_close, &door);
+            }
+            else
+            {
+                RKH_SMA_POST_FIFO(oven, &e_open, &door);
+            }
+            break;
+    }
+}
+
+void
+bsp_emitterOn(void)
+{
+    set_rgb_led(RGB_RED);
+}
+
+void
+bsp_emitterOff(void)
+{
+    set_rgb_led(RGB_BLACK);
+}
+
+/* ------------------------------ End of file ------------------------------ */
---
+++ oven/ov1/config.mk
@@ -0,0 +1,10 @@
+APP=blinky
+
+MODULES=app lpc_chip_43xx lpc_board_ciaa_edu_4337 sapi rkh bsp
+DEFINES=CORE_M4 __USE_LPCOPEN __USE_NEWLIB __EDUCIAA_EID__
+
+VERBOSE=n
+OPT=g
+USE_NANO=y
+SEMIHOST=n
+USE_FPU=y
